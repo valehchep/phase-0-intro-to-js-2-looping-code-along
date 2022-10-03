9589fce2c309f3bb02c6dd6de9ab9126ff1cb862
Moringa School <valentinecherutich7@email.com>
2022-10-03 10:39:19 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Code your solutions in this file
+const writeCards = ["Guadalupe", "Ollie", "Aki"]
+function rapcards(writeCards){
+    console.log(`Thank you, ${writeCards}, for the wonderful surprise gift!",
+    `)
+}
+for(let countDown=0; countDown < 10;  countDown++){
+    console.log(countDown)
+}
